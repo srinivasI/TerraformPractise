a499440a4bed9b20463dbd7589844ae54433e99e
SrinivasIppili <ippilisrinivas715@gmail.com>
2025-08-07 05:04:36 +0530

    day1

--- day2Setup/provider.tf
+++ day2Setup/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
    region     = "us-east-1"
-  access_key = "AKIAQKI2PBF4HXOZNPWH"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
